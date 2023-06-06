d6471416f02b118bfda29eab8fe9a71771a92c8a
Andres <andres@pulupa.com>
2023-06-05 23:02:47 -0500

    fix style error

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,7 +4,7 @@ import Search from "./Search"
 
 const Header = () => {
     return (
-        <div className="bg-neutral-900 sticky top-0 w-full">
+        <div className="bg-neutral-900 sticky top-0 w-full z-10">
             <div className="max-w-[1400px] mx-auto px-2 flex items-center" >
                 <Link to="/">
                     <img className="w-16" src={logo} alt="logo de epic movie" />
