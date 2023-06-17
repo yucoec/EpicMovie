f1ec1a7477698595cda7eb8824c99cb358868023
Andres <andres@pulupa.com>
2023-06-17 14:05:50 -0500

    Delete pagenotfound

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,8 @@
 import { HelmetProvider } from 'react-helmet-async'
-import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import EpiModal from './components/EpiModal'
 import EpisodeSerie from './components/EpisodeSerie'
 import Header from './components/Header'
-import { PageNotFound } from './components/PageNotFound'
 import DetailsMoviePage from './pages/DetailsMoviePage.jsx'
 import DetailsSeriePage from './pages/DetailsSeriePage'
 import { Home } from './pages/Home'
@@ -26,8 +25,6 @@ function App() {
           <Route path='/series/:id/:title' element={<DetailsSeriePage />} />
           <Route path='/:id/:title/:season/:episode' element={<EpisodeSerie />} />
           <Route path='/search/:keyword' element={<SearchedMovies />} />
-          <Route path="/404" element={<PageNotFound />} />
-          <Route path="*" element={<Navigate to="/404" />} />
         </Routes>
       </BrowserRouter>
     </HelmetProvider>
