5b8e71de56db6bc24429bf18fb7c14ed12187551
Andres <andres@pulupa.com>
2023-06-18 22:09:49 -0500

    Add dropdown

--- src/components/SerieDetails.jsx
+++ src/components/SerieDetails.jsx
@@ -1,15 +1,22 @@
 import { useEffect, useState } from 'react'
-import { AiFillStar } from 'react-icons/ai'
+import { AiFillCaretDown, AiFillStar } from 'react-icons/ai'
 import { BsArrowLeft } from 'react-icons/bs'
 import { useParams } from 'react-router-dom'
 import imageNotFound from '../assets/imageNotFound.png'
 import { getSerieList } from '../services/getSerieList'
 import SeasonList from './SeasonList'
 
+function obtenerPrimeraTemporada(serie) {
+    if (serie.length > 0) {
+        return serie[0].season_number;
+    }
+    return null;
+}
+
 const SerieDetails = ({ details, backPage }) => {
     const [serie, setSerie] = useState([]);
     const { id } = useParams()
-
+    const [temporadaSeleccionada, setTemporadaSeleccionada] = useState(null);
 
     useEffect(() => {
         getSerieList().then(data => {
@@ -18,12 +25,10 @@ const SerieDetails = ({ details, backPage }) => {
             });
             if (matchingSerie) {
                 setSerie(matchingSerie.seasons);
+                setTemporadaSeleccionada(obtenerPrimeraTemporada(matchingSerie.seasons));
             }
         });
     }, [id]);
-
-
-
 
     return (
         <>
@@ -53,12 +58,28 @@ const SerieDetails = ({ details, backPage }) => {
             </section>
 
             <section className='text-white h-full max-w-[1400px] mx-auto'>
+                <div className='mx-5 py-5 relative w-fit'>
+                    <select className='w-auto outline-none appearance-none bg-cyan-500 border-gray-300 rounded-md py-3 px-2 pr-10' onChange={(e) => setTemporadaSeleccionada(Number(e.target.value))}>
+                        {serie.map(({ season_number }) => (
+                            <option key={season_number} value={season_number}>
+                                Temporada {season_number}
+                            </option>
+                        ))}
+                    </select>
+                    <div className="absolute inset-y-0 right-0 flex items-center pr-3 pointer-events-none">
+                        <AiFillCaretDown />
+                    </div>
+                </div>
+
                 {serie.map(({ season_number, id: season_id, episodes }) => {
                     return (
-                        <SeasonList key={season_id} id={id} season_number={season_number} episodes={episodes} name={details.original_name} />
-                    )
+                        temporadaSeleccionada === season_number && (
+                            <SeasonList key={season_id} id={id} season_number={season_number} episodes={episodes} name={details.original_name} />
+                        )
+                    );
                 })}
             </section>
+
 
             <div className='flex justify-center my-7 text-white'>
                 <button className='flex gap-1 items-center hover:border-b-2' onClick={backPage}><BsArrowLeft />Back</button>
