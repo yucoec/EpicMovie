1374699e4152bceab62a8314f73bb933ca8c2c18
Andres <andres@pulupa.com>
2023-06-28 06:20:47 -0500

    Deleted console.log

--- src/hooks/useChangeEpisodes.js
+++ src/hooks/useChangeEpisodes.js
@@ -34,6 +34,5 @@ export function useChangeEpisodes({ id, season, episode }) {
         prevEpisode = prevIndex >= 0 ? episodes[prevIndex] : null;
         nextEpisode = nextIndex < episodes.length ? episodes[nextIndex] : null;
     }
-    console.log(prevEpisode, nextEpisode, totalEpisodes);
     return { prevEpisode, nextEpisode, totalEpisodes }
 }
