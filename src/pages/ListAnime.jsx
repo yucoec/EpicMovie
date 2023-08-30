073356451ed8f623b35ab372a12887a250b66814
Andres <andrespulupa18@gmail.com>
2023-08-30 07:35:35 -0500

    Add page 2 to series

---
+++ src/pages/ListAnime.jsx
@@ -0,0 +1,76 @@
+import React, { useEffect, useState } from 'react';
+import { useNavigate, useParams } from 'react-router-dom';
+import { MoonLoader } from 'react-spinners';
+import { Footer } from '../components/Footer';
+import { ListOfAnimes } from '../components/ListOfAnimes';
+import { getSerieList } from '../services/getSerieList';
+
+const ListAnime = () => {
+    const [movieList, setMovieList] = useState([]);
+    const [currentPage, setCurrentPage] = useState(2);
+    const navigate = useNavigate();
+    const { page } = useParams();
+    const productsPerPage = 20;
+
+    useEffect(() => {
+        getSerieList().then(data => {
+            setMovieList(data)
+        })
+    }, [])
+
+    const indexOfLastProduct = currentPage * productsPerPage;
+    const indexOfFirstProduct = indexOfLastProduct - productsPerPage;
+    const moviePage = movieList.slice(indexOfFirstProduct, indexOfLastProduct);
+    // Función para cambiar de página
+    const handlePageChange = (pageNumber) => {
+        window.scroll({
+            top: 0,
+            behavior: 'smooth',
+        });
+        navigate(`/anime/list/${Number(page) + 1}`);
+        setCurrentPage(pageNumber);
+    };
+
+    // Función para retroceder una página
+    const handlePreviousPage = () => {
+        if (currentPage === 2) {
+            navigate('/anime');
+            setCurrentPage(1);
+        } else {
+            navigate(`/anime/list/${Number(page) - 1}`);
+            setCurrentPage((prevPage) => prevPage - 1);
+        }
+    };
+
+    return (
+        <>
+            <div className='bg-black text-white'>
+                <div className='w-full max-w-[1200px] mx-auto'>
+                    {moviePage?.length > 0 ? (
+                        <>
+                            <ListOfAnimes movieList={moviePage} />
+                            <div className='flex justify-center gap-5 m-10'>
+                                {page > 1 && (
+                                    <button className='h-6 hover:border-b-slate-50 hover:border-b-2' onClick={handlePreviousPage}>
+                                        Anterior
+                                    </button>
+                                )}
+                                <p>{page}</p>
+                                {movieList.length > indexOfLastProduct && (
+                                    <button className='h-6 hover:border-b-slate-50 hover:border-b-2' onClick={() => handlePageChange(currentPage + 1)}>
+                                        Siguiente
+                                    </button>
+                                )}
+                            </div>
+                        </>
+                    ) : (
+                        <MoonLoader color='#fff' size={120} speedMultiplier={2} />
+                    )}
+                </div>
+            </div>
+            <Footer />
+        </>
+    );
+};
+
+export default ListAnime;
